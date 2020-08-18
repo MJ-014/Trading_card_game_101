7ecb24ea72e4b6bc4e4d029148edf859f4600ffc
Navid <51940962+Navid-d@users.noreply.github.com>
2020-08-18 16:01:40 +0430

    Added random types
    Now, cards can have random types

--- card.py
+++ card.py
@@ -1,13 +1,22 @@
 from random import randint
 
 class Card:
-    def __init__(self, cardType):
-        self.type = cardType
-
+    def __init__(self, cardType=""):
+        self.cardType = cardType
+    
     def chooseTypeRandomly(self):
-        types = ["Fighter", "Healer", "Trainer", "Rare", "Energy"]
+        types = ["Fighter", "Healer", "Trainer", "Rare"]
         numb = randint(0, 101)
-        if numb == 17:
-            return types[3]
-        elif numb % 10 == 0:
-            return types[0]
+
+        if numb in range(0, 51):
+            self.cardType = types[0]
+        elif numb in range(51, 76):
+            self.cardType = types[2]
+        elif numb in range(76, 100):
+            self.cardType = types[1]
+        elif numb == 100:
+            self.cardType = types[3]
+        else:
+            self.cardType = "No"
+
+
